5a0b2348536f64c2713c746d301f2786ffd7e960
Mike Schmidt <mike@blockstream.io>
2018-05-21 12:06:53 -0500

    initial project basics with tagging

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from flask_flatpages import FlatPages # pylint: disable=E0401
+from flask_frozen import Freezer # pylint: disable=E0401
+import click
+
+DEBUG = True
+FLATPAGES_AUTO_RELOAD = DEBUG
+FLATPAGES_EXTENSION = '.md'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+pages = FlatPages(app)
+freezer = Freezer(app)
+
+if __name__ == '__main__':
+  app.run()
+
+@app.route('/')
+def index():
+    return render_template('index.html', pages=pages)
+
+@app.route('/<path:path>/')
+def page(path):
+    page = pages.get_or_404(path)
+    return render_template('page.html', page=page)
+
+@app.route('/tag/<string:tag>/')
+def tag(tag):
+    tagged = [p for p in pages if tag in p.meta.get('tags', [])]
+    return render_template('tag.html', pages=tagged, tag=tag)
+
+@app.cli.command()
+def freeze():
+    freezer.freeze()
